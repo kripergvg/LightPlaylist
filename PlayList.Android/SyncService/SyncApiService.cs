dad0be366cc83f8687995050dbb24cebe490a1b2
wkololo_4ever <wkololo_4ever@mail.ru>
2016-09-25 20:51:29 +0300

    fix windows forms error

--- PlayList.Android/SyncService/SyncApiService.cs
+++ PlayList.Android/SyncService/SyncApiService.cs
@@ -1,41 +1,41 @@
-//using System;
-//using System.Globalization;
-//using System.IO;
-//using System.Net;
-//using System.Threading.Tasks;
-//using LightPlaylist.Models;
-//using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using LightPlaylist.Models;
+using Newtonsoft.Json;
 
-//namespace PlayList.Android.SyncService
-//{
-//    public class SyncApiService
-//    {
-//        public async Task<Song[]> GetNewSongs(DateTime? from)
-//        {
-//            string requestUrl = "http://kriperplaylist.azurewebsites.net/api/sync";
+namespace PlayList.Android.SyncService
+{
+    public class SyncApiService
+    {
+        public async Task<Song[]> GetNewSongs(DateTime? from)
+        {
+            string requestUrl = "http://kriperplaylist.azurewebsites.net/api/sync";
 
-//            if (from.HasValue)
-//            {
-//                requestUrl += $"?date={@from.Value.ToString("MM.dd.yyyy hh:mm:ss", CultureInfo.InvariantCulture)}";
-//            }
+            if (from.HasValue)
+            {
+                requestUrl += $"?date={@from.Value.ToString("MM.dd.yyyy hh:mm:ss", CultureInfo.InvariantCulture)}";
+            }
 
-//            var lastTracksRequest = WebRequest.Create(requestUrl);
-//            using (var sr = new StreamReader(lastTracksRequest.GetResponse().GetResponseStream()))
-//            {
-//                var response = await sr.ReadToEndAsync();
-//                return JsonConvert.DeserializeObject<Song[]>(response);
-//            }
-//        }
+            var lastTracksRequest = WebRequest.Create(requestUrl);
+            using (var sr = new StreamReader(lastTracksRequest.GetResponse().GetResponseStream()))
+            {
+                var response = await sr.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<Song[]>(response);
+            }
+        }
 
-//        public void DeleteSong(string vkKey)
-//        {
-//            string requestUrl = $"http://kriperplaylist.azurewebsites.net/api/song?vkId={vkKey}";
+        public void DeleteSong(string vkKey)
+        {
+            string requestUrl = $"http://kriperplaylist.azurewebsites.net/api/songs?vkId={vkKey}";
 
-//            var delteSongRequest = WebRequest.Create(requestUrl);
-//            delteSongRequest.Method = "DELETE";
-//            using (var sr = new StreamReader(delteSongRequest.GetResponse().GetResponseStream()))
-//            {
-//            }            
-//        }
-//    }
-//}
+            var delteSongRequest = WebRequest.Create(requestUrl);
+            delteSongRequest.Method = "DELETE";
+            using (var sr = new StreamReader(delteSongRequest.GetResponse().GetResponseStream()))
+            {
+            }
+        }
+    }
+}
